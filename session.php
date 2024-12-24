16c875a501919f2dac982da88dfbaa96362e448f
alexandr shagov <shagov25@gmail.com>
2024-12-24 10:19:42 +0300

    Подкорректировал путь перенаправления, согласно обртаной связи

--- session.php
+++ session.php
@@ -5,7 +5,7 @@ session_start();
 if(isset($_SESSION['numberOfClicks'])){
     $_SESSION['numberOfClicks']++;
     if($_SESSION['numberOfClicks'] % 3 === 0){
-        header('Location: ../report.php');
+        header('Location: report.php');
     }
 }else{
     $_SESSION['numberOfClicks'] = 1;
